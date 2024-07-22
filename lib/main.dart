d0a66a211a6c312e1af0876576afb30c225cd60d
AliAkhmat <aliakhmat31@gmail.com>
2024-07-22 18:28:01 +0300

    a add

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,3 @@
-// import 'package:audioplayers/audioplayers.dart';
-// import 'package:flutter/material.dart';
-// import 'package:xzelaphone_app/screen/list_of_surah_and_name.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Flutter Demo',
-//       theme: ThemeData(),
-//       home: const HolophoneApp(),
-//     );
-//   }
-// }
-
-// class HolophoneApp extends StatefulWidget {
-//   const HolophoneApp({super.key});
-
-//   @override
-//   State<HolophoneApp> createState() => _HolophoneAppState();
-// }
-
-// class _HolophoneAppState extends State<HolophoneApp> {
-//   final AudioPlayer _player = AudioPlayer();
-//   final SurahPlayer _mp3 = SurahPlayer();
-//   final named = SurahNamed();
-
-//   @override
-//   void dispose() {
-//     _player.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         body: SizedBox(
-//       width: double.infinity,
-//       height: 1000,
-//       child: ListView.builder(
-//         itemBuilder: (context, index) {
-//           return Column(
-//             children: [
-//               InkWell(
-//                 onTap: () async {
-//                   try {
-//                     await _player.play(AssetSource(_mp3.mp3[index]));
-//                   } catch (e) {
-//                     print('Ошибка при воспроизведении аудио: $e');
-//                   }
-//                 },
-//                 child: ListTile(
-//                   title: Text(
-//                     named.named[index],
-//                   ),
-//                 ),
-//               ),
-//               const Divider(
-//                 color: Colors.black,
-//                 height: 1,
-//               )
-//             ],
-//           );
-//         },
-//         itemCount: _mp3.mp3.length,
-//         shrinkWrap: true,
-//         scrollDirection: Axis.vertical,
-//       ),
-//     ));
-//   }
-// }
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:xzelaphone_app/screen/list_of_surah_and_name.dart';
@@ -151,45 +73,51 @@ class _HolophoneAppState extends State<HolophoneApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SizedBox(
-      width: double.infinity,
-      height: 1000,
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-          return Column(
-            children: [
-              InkWell(
-                onTap: () async {
-                  if (_currentIndex == index) {
-                    // Если нажата та же кнопка, переключаем состояние воспроизведения
-                    await _playPauseAudio(index);
-                  } else {
-                    // Запустить или перезапустить трек, если выбран новый
-                    await _restartAudio(index);
-                  }
-                },
-                child: ListTile(
-                  trailing: IconButton(
+      body: SizedBox(
+        width: double.infinity,
+        height: 1000,
+        child: ListView.builder(
+          itemCount: _mp3.mp3.length,
+          itemBuilder: (context, index) {
+            if (index >= _named.named.length) {
+              return const SizedBox.shrink();
+            }
+
+            return Column(
+              children: [
+                InkWell(
+                  onTap: () async {
+                    if (_currentIndex == index) {
+                      // Если нажата та же кнопка, переключаем состояние воспроизведения
+                      await _playPauseAudio(index);
+                    } else {
+                      // Запустить или перезапустить трек, если выбран новый
+                      await _restartAudio(index);
+                    }
+                  },
+                  child: ListTile(
+                    trailing: IconButton(
                       onPressed: () {
                         _restartAudio(index);
                       },
-                      icon: const Icon(Icons.restart_alt)),
-                  title: Text(
-                    _named.named[index],
+                      icon: const Icon(Icons.restart_alt),
+                    ),
+                    title: Text(
+                      _named.named[index],
+                    ),
                   ),
                 ),
-              ),
-              const Divider(
-                color: Colors.black,
-                height: 1,
-              )
-            ],
-          );
-        },
-        itemCount: _mp3.mp3.length,
-        shrinkWrap: true,
-        scrollDirection: Axis.vertical,
+                const Divider(
+                  color: Colors.black,
+                  height: 1,
+                )
+              ],
+            );
+          },
+          shrinkWrap: true,
+          scrollDirection: Axis.vertical,
+        ),
       ),
-    ));
+    );
   }
 }
